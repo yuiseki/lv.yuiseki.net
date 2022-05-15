0c7148f3b157625ca2765d08a892992aa31ca4ec
yuiseki <yuiseki@gmail.com>
2022-05-15 17:08:14 +0900

    ProductCellはメモ化しないほうがいい

--- src/App.tsx
+++ src/App.tsx
@@ -142,90 +142,94 @@ const ModelCell: React.FC<{ productId: string; model: any }> = React.memo(
 );
 ModelCell.displayName = "ModelCell";
 
-const ProductCell: React.FC<{ productId: string; query: string }> = React.memo(
-  ({ productId, query }) => {
-    const [shouldFetch, setShouldFetch] = useState(false);
-    const { data: productData } = useSWR(
-      shouldFetch ? `/products/${productId}.json` : null,
-      fetcher,
-      {
-        revalidateIfStale: false,
-        revalidateOnFocus: false,
-        revalidateOnReconnect: false,
-      }
-    );
-    const ref = useRef<HTMLAnchorElement>(null);
-
-    const loadData = useCallback(async () => {
-      setShouldFetch(true);
-    }, []);
-    useIntersectionObserver(ref, loadData);
-
-    if (!productData) {
-      return (
-        <a ref={ref} style={{ height: "200px" }}>
-          {productId}
-        </a>
-      );
+const ProductCell: React.FC<{ productId: string; query: string }> = ({
+  productId,
+  query,
+}) => {
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const { data: productData } = useSWR(
+    shouldFetch ? `/products/${productId}.json` : null,
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
     }
+  );
+  const ref = useRef<HTMLAnchorElement>(null);
 
-    if (
-      !productData.model ||
-      !productData.model[0] ||
-      !productData.model[0].image ||
-      !productData.model[0].image[0] ||
-      !productData.model[0].offers
-    ) {
-      console.log(productData);
-      return null;
-    }
+  const loadData = useCallback(async () => {
+    setShouldFetch(true);
+  }, []);
+  useIntersectionObserver(ref, loadData);
 
-    if (query.length > 0) {
-      if (
-        productData.model[0].name?.indexOf(query) === -1 &&
-        productData.model[0].disambiguatingDescription?.indexOf(query) === -1 &&
-        productData.model[0].category
-          ?.map((cat) => {
-            return cat.name;
-          })
-          .indexOf(query) === -1
-      ) {
-        return null;
-      }
-    }
-
+  if (!productData) {
     return (
-      <>
-        {productData &&
-          productData.model
-            .filter((model, idx) => {
-              if (idx === 0) {
-                return true;
-              } else {
-                if (model.macroColor !== productData.model[0].macroColor) {
-                  return true;
-                } else {
-                  if (model.sizeDisplayName) {
-                    return false;
-                  }
-                }
-              }
-            })
-            .map((model) => {
-              return (
-                <ModelCell
-                  key={model.identifier}
-                  productId={productId}
-                  model={model}
-                />
-              );
-            })}
-      </>
+      <a ref={ref} style={{ height: "200px" }}>
+        {productId}
+      </a>
     );
   }
-);
-ProductCell.displayName = "ProductCell";
 
+  if (
+    !productData.model ||
+    !productData.model[0] ||
+    !productData.model[0].image ||
+    !productData.model[0].image[0] ||
+    !productData.model[0].offers
+  ) {
+    console.log(productData);
+    return null;
+  }
+
+  if (query.length > 0) {
+    if (
+      !productData.model[0].name ||
+      !productData.model[0].disambiguatingDescription
+    ) {
+      return null;
+    }
+    if (
+      productData.model[0].name?.indexOf(query) === -1 &&
+      productData.model[0].disambiguatingDescription?.indexOf(query) === -1 &&
+      productData.model[0].category
+        ?.map((cat) => {
+          return cat.name;
+        })
+        .indexOf(query) === -1
+    ) {
+      return null;
+    }
+  }
+
+  return (
+    <>
+      {productData &&
+        productData.model
+          .filter((model, idx) => {
+            if (idx === 0) {
+              return true;
+            } else {
+              if (model.sizeDisplayName) {
+                return false;
+              }
+              if (model.macroColor !== productData.model[0].macroColor) {
+                return true;
+              }
+            }
+          })
+          .map((model) => {
+            return (
+              <ModelCell
+                key={model.identifier}
+                productId={productId}
+                model={model}
+              />
+            );
+          })}
+    </>
+  );
+};
 function App() {
   const [products, setProducts] = useState<string[] | undefined>(undefined);
   const [query, setQuery] = useState("");
