502627cd7d0a7f5d0d2bfc949795ab073707c063
yuiseki <yuiseki@gmail.com>
2022-11-19 01:01:47 +0000

    Headless UIでコンボボックスを実装

--- src/components/SearchQueryInput.tsx
+++ src/components/SearchQueryInput.tsx
@@ -1,53 +1,125 @@
-import { useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+const suggestWords = [
+  "ビジュー",
+  "ポルト クレ",
+  "ポルト モネ",
+  "ポルト カルト",
+  "ポルト フォイユ",
+  "ブラスレ",
+  "ブラスレ パーティ",
+  "エテュイ",
+  "ジッピー",
+  "ポシェット",
+  "ネヴァーフル",
+  "ドーフィーヌ",
+  "オン ザ ゴー",
+  "プティット マル",
+  "コフレ",
+  "フレグランス",
+  "ゲーム オン",
+  "スカーフ",
+  "ドレス",
+  "コート",
+];
 
 export const SearchQueryInput: React.FC<{
   onChange: (value: string) => void;
-}> = ({ onChange }) => {
+}> = ({ onChange: _onChange }) => {
   const [value, setValue] = useState("");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    _onChange(value);
+  }, [value]);
 
   return (
-    <>
-      <input
-        type="text"
-        placeholder={"Search items..."}
-        autoComplete="on"
-        list="search"
-        style={{
-          maxWidth: "90%",
-          minWidth: "30%",
-          fontSize: "1.5em",
-          padding: "10px",
-          backgroundColor: "#f6f5f3",
-          border: "none",
-        }}
+    <div style={{ position: "relative", top: 0 }}>
+      <Combobox
         value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-          onChange(e.target.value);
+        onChange={(selectedValue) => {
+          setIsActive(false);
+          setValue(selectedValue);
         }}
-      />
-      <datalist id="search">
-        <option value="ビジュー" />
-        <option value="ポルト クレ" />
-        <option value="ポルト モネ" />
-        <option value="ポルト カルト" />
-        <option value="ポルト フォイユ" />
-        <option value="ブラスレ" />
-        <option value="エテュイ" />
-        <option value="ジッピー" />
-        <option value="ポシェット" />
-        <option value="ジッピー" />
-        <option value="ネヴァーフル" />
-        <option value="ドーフィーヌ" />
-        <option value="オン ザ ゴー" />
-        <option value="プティット マル" />
-        <option value="コフレ" />
-        <option value="フレグランス" />
-        <option value="ゲーム オン" />
-        <option value="スカーフ" />
-        <option value="ドレス" />
-        <option value="コート" />
-      </datalist>
-    </>
+      >
+        {({ open }) => (
+          <div style={{ position: "relative" }}>
+            <div
+              style={{
+                backgroundColor: "#f6f5f3",
+              }}
+            >
+              <Combobox.Input
+                onFocus={() => !open && setIsActive(true)}
+                onBlur={() => open && setIsActive(false)}
+                displayValue={() => {
+                  return value;
+                }}
+                onChange={(e) => {
+                  setValue(e.currentTarget.value);
+                  setIsActive(e.currentTarget.value.length === 0);
+                }}
+                style={{
+                  maxWidth: "90%",
+                  minWidth: "30%",
+                  fontSize: "1.5em",
+                  padding: "10px",
+                  border: "0 none",
+                  borderRadius: 0,
+                  background: "transparent",
+                }}
+              />
+              <Combobox.Button
+                onClick={() =>
+                  isActive ? setIsActive(false) : setIsActive(true)
+                }
+                style={{
+                  background: "transparent",
+                  border: "0 none",
+                }}
+              >
+                <ChevronUpDownIcon
+                  style={{ height: "1.5em", width: "1.5em" }}
+                  aria-hidden="true"
+                />
+              </Combobox.Button>
+            </div>
+            <Transition as={Fragment} show={isActive}>
+              <Combobox.Options
+                style={{
+                  position: "absolute",
+                  background: "#fff",
+                  width: "90%",
+                  maxWidth: "90%",
+                  minWidth: "30%",
+                  maxHeight: "200px",
+                  overflowY: "auto",
+                  listStyle: "none",
+                  margin: 0,
+                  padding: 10,
+                  zIndex: 1000,
+                }}
+              >
+                {suggestWords.map((word) => (
+                  <Combobox.Option
+                    key={word}
+                    value={word}
+                    style={{
+                      position: "relative",
+                      cursor: "pointer",
+                      margin: "5px 0",
+                    }}
+                  >
+                    {word}
+                  </Combobox.Option>
+                ))}
+              </Combobox.Options>
+            </Transition>
+          </div>
+        )}
+      </Combobox>
+    </div>
   );
 };
