acab76e7b992cdb2b71d7a1355351327bb556a88
yuiseki <yuiseki@gmail.com>
2023-12-16 13:51:45 +0900

    revert scripts/build_search_csv.sh

--- scripts/build_search_csv.sh
+++ scripts/build_search_csv.sh
@@ -2,4 +2,4 @@
 ls -t -1 public/products/*.json | xargs cat | \
   jq -r '.model[] | [.productId, .name, .offers.priceSpecification.price, .macroColor, .disambiguatingDescription, ([.category[] | .name] | join(":"))] | @csv' > tmp/search.csv
 
-split -l 5000 -d tmp/search.csv public/search_ --additional-suffix=.csv
+split -l 10000 -d tmp/search.csv public/search_ --additional-suffix=.csv
