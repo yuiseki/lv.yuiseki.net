f867e9a6d3f7713fc12c10b8a93fd705081bd720
yuiseki <yuiseki@gmail.com>
2024-02-24 13:38:01 +0900

    update 2024-02-24

--- scripts/git-set-file-times.sh
+++ scripts/git-set-file-times.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-for FILE in `git ls-files`; do
+for FILE in `git ls-files | grep public/products`; do
   COMMIT_TIME=`git -c diff.renames=false log -m -r --no-color --pretty=format:%ci -z -n1 $FILE`
   echo -e "$COMMIT_TIME\t$FILE"
   TIMESTAMP=`date -d "$COMMIT_TIME" +"%y%m%d%H%M.%S"`
