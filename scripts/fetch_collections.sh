63997775ac1bb5f21d676f375a6e6157c361abec
yuiseki <yuiseki@gmail.com>
2024-08-02 09:43:52 +0900

    update user agent

--- scripts/fetch_collections.sh
+++ scripts/fetch_collections.sh
@@ -15,14 +15,18 @@ for collection in $collections; do
     time curl \
       -s \
       -o - \
-      -H 'sec-ch-ua: "Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"' \
-      -H 'sec-ch-ua-platform: "Windows"' \
+      -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+      -H 'accept-language: ja-JP,ja;q=0.9' \
+      -H 'priority: u=0, i' \
+      -H 'sec-ch-ua: "Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"' \
       -H 'sec-ch-ua-mobile: ?0' \
-      -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
-      -H 'Accept: application/json, text/plain, */*' \
-      -H 'Referer: https://jp.louisvuitton.com/jpn-jp/homepage' \
-      -H 'client_secret: 60bbcdcD722D411B88cBb72C8246a22F' \
-      -H 'client_id: 607e3016889f431fb8020693311016c9' \
+      -H 'sec-ch-ua-platform: "Windows"' \
+      -H 'sec-fetch-dest: document' \
+      -H 'sec-fetch-mode: navigate' \
+      -H 'sec-fetch-site: none' \
+      -H 'sec-fetch-user: ?1' \
+      -H 'upgrade-insecure-requests: 1' \
+      -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36' \
       $url | jq . > $firstoutfile
   fi
 
