8c839b63bd7605929ac977f63d03ad95acda0b2e
yuiseki <yuiseki@gmail.com>
2024-10-28 07:51:01 +0900

    update 2024-10-28

--- scripts/fetch_collections.sh
+++ scripts/fetch_collections.sh
@@ -1,6 +1,9 @@
 
 collections=`cat ./tmp/collections_uniq.txt`
 
+curl 'https://api.louisvuitton.com/eco-eu/search-merch-eapi/v1/jpn-jp/plp/products/td4mq4v?page=0&attributes=brandContent&viewMore=true' \
+
+
 IFS=$'\n'
 for collection in $collections; do
   echo "----- ----- ----- -----"
@@ -8,28 +11,29 @@ for collection in $collections; do
 
   firstoutfile="./tmp/collections/$collection-0.json"
   if [ ! -s $firstoutfile ]; then
-    url="https://api.louisvuitton.com/eco-eu/search-merch-eapi/v1/jpn-jp/plp/products/$collection?page=0&attributes=brandContent"
+    url="https://api.louisvuitton.com/eco-eu/search-merch-eapi/v1/jpn-jp/plp/products/$collection?page=0&attributes=brandContent&viewMore=true"
     echo $url
     sleep 0.1
 
     time curl \
       -s \
       -o - \
-      -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
-      -H 'accept-language: ja-JP,ja;q=0.9' \
-      -H 'priority: u=0, i' \
-      -H 'sec-ch-ua: "Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"' \
-      -H 'sec-ch-ua-mobile: ?0' \
-      -H 'sec-ch-ua-platform: "Windows"' \
-      -H 'sec-fetch-dest: document' \
-      -H 'sec-fetch-mode: navigate' \
-      -H 'sec-fetch-site: none' \
-      -H 'sec-fetch-user: ?1' \
-      -H 'upgrade-insecure-requests: 1' \
-      -H 'referer: https://jp.louisvuitton.com/jpn-jp/sitemap' \
+      -H 'accept: application/json, text/plain, */*' \
+      -H 'accept-language: ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7' \
+      -H 'cache-control: no-cache' \
       -H 'client_id: 607e3016889f431fb8020693311016c9' \
       -H 'client_secret: 60bbcdcD722D411B88cBb72C8246a22F' \
-      -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36' \
+      -H 'origin: https://jp.louisvuitton.com' \
+      -H 'pragma: no-cache' \
+      -H 'priority: u=1, i' \
+      -H 'referer: https://jp.louisvuitton.com/jpn-jp/homepage' \
+      -H 'sec-ch-ua: "Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"' \
+      -H 'sec-ch-ua-mobile: ?0' \
+      -H 'sec-ch-ua-platform: "Windows"' \
+      -H 'sec-fetch-dest: empty' \
+      -H 'sec-fetch-mode: cors' \
+      -H 'sec-fetch-site: same-site' \
+      -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' \
       $url | jq . > $firstoutfile
   fi
 
@@ -45,28 +49,29 @@ for collection in $collections; do
     outfile="./tmp/collections/$collection-$i.json"
     echo $outfile
     if [ ! -s $outfile ]; then
-      url="https://api.louisvuitton.com/eco-eu/search-merch-eapi/v1/jpn-jp/plp/products/$collection?page=$i&attributes=brandContent"
+      url="https://api.louisvuitton.com/eco-eu/search-merch-eapi/v1/jpn-jp/plp/products/$collection?page=$i&attributes=brandContent&viewMore=true"
       echo $url
       sleep 0.1
 
       time curl \
         -s \
         -o - \
-        -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
-        -H 'accept-language: ja-JP,ja;q=0.9' \
-        -H 'priority: u=0, i' \
-        -H 'sec-ch-ua: "Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"' \
-        -H 'sec-ch-ua-mobile: ?0' \
-        -H 'sec-ch-ua-platform: "Windows"' \
-        -H 'sec-fetch-dest: document' \
-        -H 'sec-fetch-mode: navigate' \
-        -H 'sec-fetch-site: none' \
-        -H 'sec-fetch-user: ?1' \
-        -H 'upgrade-insecure-requests: 1' \
-        -H 'referer: https://jp.louisvuitton.com/jpn-jp/sitemap' \
+        -H 'accept: application/json, text/plain, */*' \
+        -H 'accept-language: ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7' \
+        -H 'cache-control: no-cache' \
         -H 'client_id: 607e3016889f431fb8020693311016c9' \
         -H 'client_secret: 60bbcdcD722D411B88cBb72C8246a22F' \
-        -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36' \
+        -H 'origin: https://jp.louisvuitton.com' \
+        -H 'pragma: no-cache' \
+        -H 'priority: u=1, i' \
+        -H 'referer: https://jp.louisvuitton.com/jpn-jp/homepage' \
+        -H 'sec-ch-ua: "Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"' \
+        -H 'sec-ch-ua-mobile: ?0' \
+        -H 'sec-ch-ua-platform: "Windows"' \
+        -H 'sec-fetch-dest: empty' \
+        -H 'sec-fetch-mode: cors' \
+        -H 'sec-fetch-site: same-site' \
+        -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' \
         $url | jq . > $outfile
     fi
     i=$((i+1))
